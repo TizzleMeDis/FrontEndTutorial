abca8fcc9eb5aec8d58b4548d37f80e57fb617e5
TizzleMeDis <anthonytisdale19@gmail.com>
2021-11-10 20:03:06 -0800

    Latest update of repos. Making customizable menu bar with javascript. Also new play website called cyphercams

---
+++ Practice/Small portions/MenuBar/menuBar.js
@@ -0,0 +1,10 @@
+document.getElementById("demo").innerHTML = ("Hello World!");
+
+function menuLinks(){
+	document.write("<ul class='template'>"
+		+ "<li>Home</li>"
+		+ "<li>Projects</li>"
+		+ "<li>Contact</li>"
+		+ "<li>Git Hub</li>" 
+		+ "</ul>");
+}
