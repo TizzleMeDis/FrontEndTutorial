a150758ce25ff838495ba4ef10f01fb6c4a77424
TizzleMeDis <anthonytisdale19@gmail.com>
2021-11-12 00:16:51 -0800

    menu bar javascript file was added to

--- Practice/Small portions/MenuBar/menuBar.js
+++ Practice/Small portions/MenuBar/menuBar.js
@@ -1,3 +1,5 @@
+
+
 document.getElementById("demo").innerHTML = ("Hello World!");
 
 function menuLinks(){
@@ -9,9 +11,20 @@ function menuLinks(){
 		+ "</ul>");
 }
 
+//changes the look of the html for different views of the navbar
+
+
+
+
+
+//sets of colors that work well with each other
 const colorWheel = {"#0B0C10", "#1F2833", "#C5C6C7", "#66FCF1", "#45A29E", 
 					"#5D001E", "#E3E2DF", "#E3AFBC", "#9A1750", "#EE4C7C", 
 					"#EDC7B7", "#EEE2DC", "#BAB2B5", "#123C69", "#AC3B61"}
 
-const colorScheme = {colorWheel};
-function changeColor() {}
+
+
+
+const changeColor = (header, body, containers, accents, text) = {
+	//This is where you add the color and it will link to the dom of the css
+}
