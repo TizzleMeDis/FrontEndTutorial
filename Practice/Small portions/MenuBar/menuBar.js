85cb263fe04b2e583b2b7e804b6d378728d2b95c
TizzleMeDis <anthonytisdale19@gmail.com>
2021-11-11 20:24:51 -0800

    Another update for dirty commits

--- Practice/Small portions/MenuBar/menuBar.js
+++ Practice/Small portions/MenuBar/menuBar.js
@@ -8,3 +8,10 @@ function menuLinks(){
 		+ "<li>Git Hub</li>" 
 		+ "</ul>");
 }
+
+const colorWheel = {"#0B0C10", "#1F2833", "#C5C6C7", "#66FCF1", "#45A29E", 
+					"#5D001E", "#E3E2DF", "#E3AFBC", "#9A1750", "#EE4C7C", 
+					"#EDC7B7", "#EEE2DC", "#BAB2B5", "#123C69", "#AC3B61"}
+
+const colorScheme = {colorWheel};
+function changeColor() {}
